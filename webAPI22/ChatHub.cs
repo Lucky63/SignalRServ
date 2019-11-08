79c046d6898f41caf9de642be9bb5dfab3e4a192
liceist2008@mail.ru <liceist2008@mail.ru>
2019-11-08 16:27:51 +0600

    Добавьте файлы проекта.

---
+++ webAPI22/ChatHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webAPI22
+{	
+	public class ChatHub : Hub
+	{
+		public async Task Send(string message)
+		{
+			await this.Clients.All.SendAsync("Send", message);
+		}
+
+		public void SendToAll(string name, string message)
+		{
+			Clients.All.SendAsync("sendToAll", name, message);
+		}
+	}
+}
---
+++ webAPI22/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace webAPI22
+{
+	public class Startup
+	{
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddSignalR();
+			services.AddCors(o => o.AddPolicy("CorsPolicy", builder =>
+			{
+				builder
+					.WithOrigins("http://localhost:4200")
+					.AllowAnyMethod()
+					.AllowAnyHeader()
+					.AllowCredentials(); 
+			}));
+			
+		}
+
+		public void Configure(IApplicationBuilder app)
+		{
+			app.UseCors("CorsPolicy");
+			app.UseSignalR(routes =>
+			{
+				routes.MapHub<ChatHub>("/chat");
+			});
+
+			app.UseDefaultFiles();
+			app.UseStaticFiles();
+
+			
+		}
+	}
+}
